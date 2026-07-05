5c822bde4991f6891e0ddc57ce90e3ed25dbf44e
moyo-1234 <folake.abudiore@gmail.com>
2026-07-05 20:06:19 +0100

    Add files via upload

--- basicfunct.py
+++ basicfunct.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 rimg = cv2.imread(r"C:\Users\femia\Desktop\python_game_dev\Opencv\rugby.png",1)
 cv2.imshow("Rugby Ball",rimg)
@@ -14,9 +15,36 @@ crimg = cv2.resize(cimg,(300,300))
 cv2.imshow("cricket ball",crimg)
 cv2.waitKey(0)
 
-var = cv2.addWeighted(crimg,0.5,rrimg,0.6,0)
+var = cv2.addWeighted(crimg,0.2,rrimg,0.8,0)
 cv2.imshow("weight",var)
 cv2.waitKey(0)
 subres = cv2.subtract(rrimg,crimg)
 cv2.imshow("subresult",subres)
 cv2.waitKey(0)
+
+# print(rimg)
+k = np.ones((1,1),np.uint8)
+resimg = cv2.erode(rrimg,k) 
+cv2.imshow("rugby ball",resimg)
+cv2.waitKey(0)
+
+gb = cv2.GaussianBlur(crimg,(15,15),1)
+cv2.imshow("Blurred image",gb)
+cv2.waitKey(0)
+
+mb = cv2.medianBlur(rrimg,15)
+cv2.imshow("Blurred image",mb)
+cv2.waitKey(0)
+
+bb = cv2.bilateralFilter(crimg,18,100,70)
+cv2.imshow("bilateral image",bb)
+cv2.waitKey(0)
+
+bi = cv2.copyMakeBorder(rrimg,15,15,15,15,cv2.BORDER_CONSTANT,value = (0,0,0))
+cv2.imshow("Bordered image",bi)
+cv2.waitKey(0)
+
+rbi = cv2.copyMakeBorder(rrimg,15,15,15,15,cv2.BORDER_REFLECT)
+cv2.imshow("Bordered image",rbi)
+cv2.waitKey(0)
+
